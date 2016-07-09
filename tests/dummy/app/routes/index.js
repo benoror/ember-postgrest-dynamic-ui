43fec19200612b1ff8b907892cfb7c0353e7ff88
Ben Orozco <benoror@gmail.com>
2016-07-09 13:12:58 -0500

    feature: add tables-menu component

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,16 +1,22 @@
 import Ember from 'ember';
 
 const {
-  get,
-  set
+  RSVP,
+  get
 } = Ember;
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
   model() {
-    const tables = get(this, 'ajax').request('http://localhost:3000/');
-    return set(this, 'tables', tables);
+    return RSVP.hash({
+      tables: get(this, 'ajax').request('http://localhost:3000/')
+    });
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.setProperties(model);
   }
 
 });
