a4f65eea1b3ee516de2bcb86ccb11ff1a7aa2203
Ben Orozco <benoror@gmail.com>
2016-07-09 12:57:26 -0500

    🎉First commit: adapter/serializer boilerplate

---
+++ tests/dummy/app/adapters/application.js
@@ -0,0 +1,3 @@
+import PostgRESTAdapter from "ember-postgrest/adapter";
+
+export default PostgRESTAdapter.extend();
---
+++ tests/dummy/app/serializers/application.js
@@ -0,0 +1,5 @@
+import PostgRESTSerializer from "ember-postgrest/serializer";
+
+export default PostgRESTSerializer.extend({
+  host: 'http://localhost:3000'
+});
