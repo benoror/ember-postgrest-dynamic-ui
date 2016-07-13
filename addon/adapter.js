28a4dd882de4f6de3436edab3b532682c35d87f0
Ben Orozco <benoror@gmail.com>
2016-07-13 18:56:16 -0500

    WIP: addon adapter/serializer
    - ON-HOLD: Ember Data doesn't support well the idea of dynamic models

--- addon/adapter.js
+++ addon/adapter.js
@@ -1,3 +1,12 @@
 import DS from "ember-data";
 
-export default DS.RESTAdapter.extend();
+export default DS.RESTAdapter.extend({
+  buildURL(modelName, id, snapshot, requestType, query) {
+    const newModelNAme = id;
+    return this._super(newModelNAme, undefined, snapshot, requestType, query);
+  }
+
+  // pathForType: function(modelName) {
+  //   return modelName;
+  // }
+});
--- addon/serializer.js
+++ addon/serializer.js
@@ -1,3 +1,39 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend();
+// const inflector = new Ember.Inflector();
+
+export default DS.JSONSerializer.extend({
+  // normalizeResponse(store, type, payload) {
+    //  const modelNamePlural = inflector.pluralize(type.modelName);
+    // const newPayload = { payload: payload };
+    // console.log(newPayload);
+
+  //
+  //   if(payload.records) {
+  //     payload[modelNamePlural] = payload.records;
+  //     delete payload.records;
+  //
+  //     payload.meta = {
+  //       offset: payload.offset
+  //     };
+  //     delete payload.offset;
+  //
+  //     payload[modelNamePlural].forEach((record) => {
+  //       Ember.merge(record, record.fields);
+  //       delete record.fields;
+  //       record.created = record.createdTime;
+  //       delete record.createdTime;
+  //     });
+  //   } else {
+  //     payload[type.modelName] = payload.fields;
+  //     payload[type.modelName].id = payload.id;
+  //     payload[type.modelName].created = payload.createdTime;
+  //     delete payload.id;
+  //     delete payload.fields;
+  //     delete payload.createdTime;
+  //   }
+  //
+    // return this._super(store, type, payload);
+  // }
+});
